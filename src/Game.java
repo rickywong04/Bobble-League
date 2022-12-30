6a51627dd7c71a4650fd6bda42cba2d432f35416
Ricky Wong <go.rickyw@gmail.com>
2022-12-30 13:08:41 -0500

    Makes a player class to keep track of its team's score

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,26 @@
-public class Game {
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JFrame implements Runnable {
+    private static JFrame frame = new JFrame();
+    private static Container content;
+
+
+    @Override
+    public void run() {
+       gameStart();
+    }
+
+    public static void gameStart(){
+        //Set the Frame
+        frame.setVisible(true);
+        frame.setSize(1280,720);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        content = frame.getContentPane();
+        content.setLayout(null);
+        frame.setLocationRelativeTo(null);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Game());
+    }
 }
---
+++ src/Player.java
@@ -0,0 +1,16 @@
+public class Player {
+    private String team;
+    private int score;
+
+    /**
+     * Creates a Player object
+     *
+     * @param team  team name for the player
+     * @param score score for the player
+     */
+    public Player(String team, int score) {
+        this.team = team;
+        this.score = score;
+
+    }
+}
